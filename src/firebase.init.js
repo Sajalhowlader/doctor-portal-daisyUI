7e54bf801bf2c8e92022f6b78ee444bcebe7db16
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-13 18:25:49 +0600

    re inisialize node moduouls

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:AIzaSyCAy1IA7klTDryFnM7eCkNrHlpP7HWGV9A,
+  authDomain:doctor-portal-bcd6e.firebaseapp.com,
+  projectId:doctor-portal-bcd6e,
+  storageBucket:doctor-portal-bcd6e.appspot.com,
+  messagingSenderId:642647757784,
+  appId:1:642647757784:web:c4b8101b39c212e692d8a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
